51bef5041559aedfba05df2f8bd44a4a5fbbede0
CodigoDeSenior <migueljnm840@gmail.com>
2022-11-06 10:25:06 -0300

    leetcode cpp

---
+++ leetcode/orderly-queue.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k>1){
+            sort(s.begin(),s.end());
+            return s;
+        }else{
+
+            string temp=s;
+            s+=s;
+            for(int i=1;i<temp.length();i++){
+                temp=min(temp,s.substr(i,temp.size()));
+            }
+            return temp;
+        }
+    }
+};
