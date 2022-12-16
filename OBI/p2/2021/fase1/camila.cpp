a36f442715c253ecf1b2eb5a7eb27f3ede6e670d
CodigoDeSenior <migueljnm840@gmail.com>
2022-12-16 11:07:11 -0300

    OBI P2 F1 2022 Camila

---
+++ OBI/p2/2021/fase1/camila.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+    
+    sort(arr, arr+3);
+
+    cout << arr[1] << "\n";
+
+}
