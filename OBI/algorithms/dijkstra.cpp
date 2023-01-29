824ec65503237ee9bd5828111467babf227ba54c
CodigoDeSenior <migueljnm840@gmail.com>
2023-01-29 14:08:04 -0300

    dijkstra attempt 2

--- OBI/algorithms/dijkstra.cpp
+++ OBI/algorithms/dijkstra.cpp
@@ -1,67 +1,114 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <iterator>
-#include <utility>
-#include <list>
 
 using namespace std;
-#define INF 0x3f3f3f3f
-
-int V = 9;
-
-list<pair<int,int>>* graph;
 
 
-void shortestPath(int src)
+using namespace std;
+
+#define SIZE 10
+
+int graph[SIZE][SIZE];
+
+void startGraph()
 {
-    priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-    vector<int> dist(V, INF);
-    dist[src] = 0;
-
-    while(!pq.empty()) {
-	int u = pq.top().second;
-	pq.pop();
-	list<pair<int, int>>::iterator i;
-
-	for (i = graph[u].begin(); i != graph[u].end(); ++i)
+    for (int i = 0; i < SIZE; i++)
+    {
+	for (int j = 0; j < SIZE; j++)
 	{
-	    int v = (*i).first;
-	    int weight = (*i).second;
-	    if (dist[v] > dist[u] + weight) {
-		dist[v] = dist[u] + weight;
-		pq.push(make_pair(dist[v], v));
+	    graph[i][j] = 0;
+	}
+    }
+}
+
+void addEdge(int i, int j, int w, bool bi)
+{
+    if (i >= SIZE || j >= SIZE)
+    {
+	cout << "UPDATE SIZE\n";
+	return;
+    }
+
+    graph[i][j] = w;
+    if (bi)
+	graph[j][i] = w; 
+
+}
+
+void printGraph()
+{
+    cout << "   ";
+    for (int i = 0; i < SIZE; i++)
+	cout << i << " ";
+
+    cout << "\n";
+
+    for (int i = 0; i <= SIZE; i++)
+	cout << "__";
+
+    cout << "\n";
+
+
+    for (int i = 0; i < SIZE; i++)
+    {
+	cout << i << "| ";
+	for (int j = 0; j < SIZE; j++)
+	{
+	    cout << graph[i][j] << " ";
+	}
+	cout << "\n";
+    }
+}
+
+bool visitedBFS[SIZE];
+
+void dijkstra(int v)
+{
+    priority_queue<int> Q; 
+    Q.push(v);
+
+    visitedBFS[v] = true;
+    
+    while(!Q.empty())
+    {
+	v = -1 * Q.top();
+	Q.pop();
+
+	cout << v << ", ";
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+	    if (graph[v][i] != 0 && !visitedBFS[i])
+	    {
+		visitedBFS[i] = true;
+		Q.push(i * -1);
 	    }
 	}
     }
-    printf("Vertex Distance from Source\n");
-    for (int i = 0; i < V; ++i)
-        printf("%d \t\t %d\n", i, dist[i]);
 }
+
 
 int main()
 {
-    graph = new list<pair<int, int>>[V];
+    
+    startGraph();
 
+    addEdge(1, 2, 1*2, true);
+    addEdge(1, 4, 2*2, true);
+    addEdge(1, 7, 3*2, true);
+    addEdge(2, 3, 4*2, true);
+    addEdge(4, 5, 5*2, true);
+    addEdge(4, 6, 6*2, true);
+    addEdge(7, 8, 7*2, true);
+    addEdge(0, 1, 8*2, true);
+    
+    printGraph();
 
-    graph[0].push_back(make_pair(1, 4));
-    graph[0].push_back(make_pair(7, 8));
-    graph[1].push_back(make_pair(2, 8));
-    graph[1].push_back(make_pair(7, 11));
-    graph[2].push_back(make_pair(3, 7));
-    graph[2].push_back(make_pair(8, 2));
-    graph[2].push_back(make_pair(5, 4));
-    graph[3].push_back(make_pair(4, 9));
-    graph[3].push_back(make_pair(5, 14));
-    graph[4].push_back(make_pair(5, 10));
-    graph[5].push_back(make_pair(6, 2));
-    graph[6].push_back(make_pair(7, 1));
-    graph[6].push_back(make_pair(8, 6));
-    graph[7].push_back(make_pair(8, 7));
-
-
-    shortestPath(0);
+    cout << "\n\n";
+    
+    dijkstra(0);
 
     return 0;
+
 }
 
