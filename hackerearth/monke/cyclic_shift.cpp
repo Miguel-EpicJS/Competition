7721e3b0243fb6691c9891c540b808491284c0d0
CodigoDeSenior <migueljnm840@gmail.com>
2023-02-02 14:55:42 -0300

    https://www.hackerearth.com/practice/codemonk/

---
+++ hackerearth/monke/cyclic_shift.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+
+    int n, k;
+
+    cin >> n >> k;
+
+    string a;
+
+    cin >> a;
+
+    cout << "\n";
+}
+
+int main() {
+    int num;
+    cin >> num; 
+
+    while(num--)
+    {
+        solve();
+    }
+
+}
