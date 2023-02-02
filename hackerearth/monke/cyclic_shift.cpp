205b89eda03a88fb0a7aec55941020740c11ff13
CodigoDeSenior <migueljnm840@gmail.com>
2023-02-02 16:11:32 -0300

    https://www.hackerearth.com/practice/codemonk/

--- hackerearth/monke/cyclic_shift.cpp
+++ hackerearth/monke/cyclic_shift.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
 using namespace std;
+
+string leftrotate(string str1, int n)
+{
+ 
+    string temp = str1 + str1;
+    int l1 = str1.size();
+ 
+    string Lfirst = temp.substr(n, l1);
+ 
+    return Lfirst;
+}
 
 void solve()
 {
@@ -12,7 +28,33 @@ void solve()
 
     cin >> a;
 
-    cout << "\n";
+    string sTop, temp;
+    int iTop = 0, mov = 0;
+    int rep = n;
+
+    for (int i = 0; i < n; i++)
+    {
+	temp = leftrotate(a, i);
+	int val = stoi(temp, nullptr, 2);
+
+	if (val > iTop)
+	{
+	    iTop = val;
+	    sTop = temp; 
+	    mov = i;
+	}
+
+	if (a.compare(temp) == 0 && i != 0)
+	{
+	    rep = 2;
+	    break;
+	}
+
+
+    }
+
+    cout << mov + (rep * (k-1)) << "\n";
+
 }
 
 int main() {
