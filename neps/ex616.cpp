8786538bcade79cf5c6a2a8afb0040bee728d214
CodigoDeSenior <migueljnm840@gmail.com>
2023-04-02 16:52:37 -0300

    neps

---
+++ neps/ex616.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char g[252][252];
+
+void dfs(int x, int y)
+{
+    if (x )
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int r, c;
+    int l = 0;
+    int o = 0;
+
+    cin >> r >> c;
+
+    for (int i = 0; i < r; i++)
+    {
+	for (int j = 0; j < c; j++)
+	{
+	    cin >> g[i][j];
+	}
+    }
+
+    return 0;
+}
+
