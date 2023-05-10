404ff42c5510183eb7e8d979a6683ca0751e96ec
CodigoDeSenior <migueljnm840@gmail.com>
2023-05-10 08:45:52 -0300

    obi

---
+++ OBI/p2/2018/fase2/elevador.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    vector<int> pesos;
+
+    for (int i = 0; i < n; i++)
+    {
+	int x;
+	cin >> x;
+	pesos.push_back(x);
+    }
+
+    pesos.push_back(0);
+
+    sort(pesos.begin(), pesos.end());
+
+    string ans = "S\n";
+
+    for (int i = 0; i < n-1; i++)
+    {
+	if (pesos[i+1] - pesos[i] > 8)
+	    ans = "N\n";
+    }
+
+    cout << ans;
+
+    return 0;
+}
+
