8823335200bcbdc1967fd5fee1aac49314985a79
CodigoDeSenior <migueljnm840@gmail.com>
2023-05-19 05:41:33 -0300

    https://leetcode.com/problems/is-graph-bipartite/solutions/115543/java-python-dfs-solution/

---
+++ leetcode/graph_bipartite.py
@@ -0,0 +1,20 @@
+def isBipartite(self, graph):
+        color = {}
+        def dfs(pos):
+            for i in graph[pos]:
+                if i in color:
+                    if color[i] == color[pos]:
+                        return False
+                else:
+                    color[i] = 1 - color[pos]
+                    if not dfs(i):
+                        return False
+            return True
+        for i in range(len(graph)):
+            if i not in color:
+                color[i] = 0
+                if not dfs(i):
+                    return False
+        return True
+
+# https://leetcode.com/problems/is-graph-bipartite/solutions/115543/java-python-dfs-solution/
