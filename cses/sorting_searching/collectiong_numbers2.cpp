d9d1b06295f588f636df7a0f97bffaf8adb30409
CodigoDeSenior <migueljnm840@gmail.com>
2023-06-30 06:34:29 -0300

    a few AC and WA

--- cses/sorting_searching/collectiong_numbers2.cpp
+++ cses/sorting_searching/collectiong_numbers2.cpp
@@ -8,17 +8,18 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
+    int n, m;
 
-    cin >> n;
+    cin >> n >> m;
 
-    vector<int> pos(n+1);
+    vector<int> pos(n+1), at(n+1);
 
     for (int i = 1; i <= n; i++)
     {
 	int x;
 	cin >> x;
 	pos[x] = i;
+	at[i] = x;
     }
 
     int rounds = 1;
@@ -28,7 +29,22 @@ int main()
 	if (pos[i] < pos[i-1]) rounds++;
     }
 
-    cout << rounds << "\n";
+    for (int i = 0; i < m; i++)
+    {
+	int x, y;
+	cin >> x >> y;
+
+	if (x > y) swap(x, y);
+
+	int px = at[x], py = at[y];
+
+	swap(at[x], at[y]);
+	swap(pos[px], pos[py]);
+   
+	if (pos[i] < pos[i-1]) rounds++;
+
+	cout << rounds << "\n";
+    }
 
     return 0;
 }
