55d48163421b2bb1ed6e6c4043ba862b861801ef
CodigoDeSenior <migueljnm840@gmail.com>
2023-08-29 05:39:16 -0300

    pilha obi 2022

--- OBI/p2/2022/fase3/pilha.cpp
+++ OBI/p2/2022/fase3/pilha.cpp
@@ -1,63 +1,58 @@
-// versão iterativa baseado em
-// versão recursiva: https://noic.com.br/materiais-informatica/comentario/obi-2022/fase3-p2/
-
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define int long long
+#define fr first
+#define sc second
+#define pi pair <int, int>
+#define pb push_back
+#define ms(v) memset(v, -1, sizeof v)
+#define srt(v) sort(v.begin(), v.end())
+#define rvs(v) reverse(v.begin(), v.end())
+#define st(v, comp) sort(v.begin(), v.end(), comp)
+#define N 510
+#define INF 1250000
 
 using namespace std;
 
-const int inf = 1234567;
-const int MAXN = 510;
+int n;
+int v[N];
+int dp[N][N];
+int k;
+int pref[N];
 
-int dp[MAXN][MAXN];
+int solve(int pos, int atk){
+    if(pos == 0) return dp[pos][atk] = 0;
+    if(atk == 0) return dp[pos][atk] = INF;
+    if(pos < atk) return dp[pos][atk] = INF;
+    if(dp[pos][atk] != -1) return dp[pos][atk];
+    dp[pos][atk] = INF;
 
-int n, k;
+    for(int i = 1;i <= pos;i++){
+        dp[pos][atk] = min(solve(pos-i, atk-1) + i*v[pos] - pref[pos] + pref[pos-i], dp[pos][atk]);
+    }
 
-int arr[MAXN], pref[MAXN];
+    return dp[pos][atk];
+}
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+int32_t main(){
     cin >> n >> k;
-    for (int j = 0; j <= k; j++)
-    {
-	for (int i = 0; i <= n; i++)
-	{
-	    dp[i][j] = inf;
-	}
+
+    for(int i = 1;i <= n;i++){
+        cin >> v[i];
     }
 
-    
-    for (int i = 1; i <= n; i++)
-	cin >> arr[i];
-    
-    sort(arr, arr+n+1);
-    pref[0] = arr[0];
+    sort(v, v+n+1);
 
-    for (int i = 1; i <= n; i++)
-	pref[i] = arr[i] + pref[i-1];
+    pref[0] = v[0];
+    ms(dp);
 
-    for (int i = 0; i <= n; i++)
-	dp[0][i] = 0;
-
-
-    for (int pos = 1; pos <= n; pos++)
-    {
-	for (int atk = 1; atk <= k; atk++)
-	{
-	    for (int i = 1; i <= pos; i++)
-	    {
-		dp[pos][atk] = min(dp[pos-i][atk-1] + i*arr[pos] - pref[pos] + pref[pos-i], dp[pos][atk]);
-	    }
-	}
+    for(int i = 1;i <= n;i++){
+        pref[i] = pref[i-1] + v[i];
     }
+
+    solve(n, k);
 
     cout << dp[n][k] << "\n";
 
-
     return 0;
 }
-
+//https://noic.com.br/materiais-informatica/comentario/obi-2022/fase3-p2/
