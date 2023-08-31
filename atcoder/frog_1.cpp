893d304cbdae0bde0e44bc925156a797638f838d
CodigoDeSenior <migueljnm840@gmail.com>
2023-08-31 05:42:21 -0300

    frog 1

--- atcoder/frog_1.cpp
+++ atcoder/frog_1.cpp
@@ -1,33 +1,25 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-int arr[100001];
-
-int solve(int i, int n)
-{
-    if (i == n) return 0;
-    if (i > n) return 100000;
-
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n;
-
-    cin >> n;
-
-    for (int i = 1; i <= n; i++)
-    {
-	cin >> arr[i];
+#define endl "\n"
+int main() {
+    
+    int N;
+    
+    cin >> N;
+    vector<int> nums(N);
+    
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+    
+    vector<int> DP(N, 0);
+    
+    DP[1] = abs(nums[1] - nums[0]);
+    
+    for (int i = 2; i < N; i++) {
+        DP[i] = min((DP[i - 1] + abs(nums[i] - nums[i - 1])), (DP[i - 2] + abs(nums[i] - nums[i - 2])));
+        
     }
 
-
-
-    return 0;
+    cout << DP[N-1] << endl;
 }
-
