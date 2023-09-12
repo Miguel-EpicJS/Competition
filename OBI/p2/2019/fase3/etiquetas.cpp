5d20204767239c404789b693b33be111ad09a958
CodigoDeSenior <migueljnm840@gmail.com>
2023-09-12 06:01:48 -0300

    some changes

--- OBI/p2/2019/fase3/etiquetas.cpp
+++ OBI/p2/2019/fase3/etiquetas.cpp
@@ -2,23 +2,13 @@
 
 using namespace std;
 
-int dp[10001][10001], arr[10001];
+int arr[100010];
 
-int n, c, k;
-const int inf = 1e8+10;
+int n, k, c;
 
-int solve(int i, int j)
+int solve(int i, int ki)
 {
-    if (i <= 0 && j == k) return 0;
-    if (i <= 0 && j != k) return inf;
 
-    if (dp[i][j] != 4*inf) return dp[i][j];
-
-    int tot = solve(i-1, j);
-
-    if (j < k && i >= c) tot = min(tot, solve(i-c, j+1)+arr[i] - arr[max(i-c, 0)]);
-
-    return dp[i][j] = tot;
 }
 
 int main()
@@ -28,7 +18,9 @@ int main()
     cout.tie(NULL);
 
 
-    cin >> n >> c >> k;
+
+    cin >> n >> k >> c;
+
 
     arr[0] = 0;
 
@@ -36,14 +28,7 @@ int main()
     {
 	cin >> arr[i];
 	arr[i]+=arr[i-1];
-
-	for(int j=0; j<=k; j++){
-            dp[i][j] = 4*inf;
-        }
     }
-
-
-    cout << arr[n] - solve(n, 0) << "\n";
 
 
     return 0;
