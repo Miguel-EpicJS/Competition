3863437e5737e0ea5f1d16b938e7fe95375c5270
CodigoDeSenior <migueljnm840@gmail.com>
2023-09-13 06:05:56 -0300

    WA

--- OBI/p2/2019/fase3/etiquetas.cpp
+++ OBI/p2/2019/fase3/etiquetas.cpp
@@ -2,34 +2,38 @@
 
 using namespace std;
 
-int arr[100010];
+const int maxn = 1e4+10, inf = 1e8+10;
 
-int n, k, c;
+int n, k, c, pref[maxn], dp[maxn][maxn];
 
-int solve(int i, int ki)
-{
+inline int solve(int i, int j){
+    if(i <= 0 and j == k) return 0;
+    if(i <= 0 and j != k) return inf;
+    if(dp[i][j] != 4*inf) return dp[i][j];
 
+    int tot = solve(i-1, j);
+
+    if(j<k and i>=c) tot = min(tot, solve(i-c, j+1) + pref[i] - pref[max(i-c, 0)]);
+
+    return dp[i][j] = tot;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
     cin >> n >> k >> c;
 
+    for(int i=1; i<=n; i++){
+        cin >> pref[i];
 
-    arr[0] = 0;
+        pref[i] += pref[i-1];
 
-    for (int i = 1; i <= n; i++)
-    {
-	cin >> arr[i];
-	arr[i]+=arr[i-1];
+        for(int j=0; j<=k; j++){
+            dp[i][j] = 4*inf;
+        }
     }
 
+    cout << pref[n] - solve(n, 0) << endl;
 
     return 0;
 }
---
+++ OBI/p2/2019/fase3/mesa.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    a = a % 3;
+
+    b = b % 3;
+
+    
+
+    return 0;
+}
+
