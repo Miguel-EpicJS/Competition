1f0d8ced5d480a68f96b8536b4a447909c1a8895
CodigoDeSenior <migueljnm840@gmail.com>
2023-09-17 18:09:04 -0300

    some changes

---
+++ OBI/p2/2009/fase2/simulador.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+
+    cin >> n >> m;
+
+
+
+    return 0;
+}
+
