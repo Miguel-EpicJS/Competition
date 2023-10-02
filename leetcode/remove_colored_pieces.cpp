4fccefdc47c5ee0a3612cb1bbbea4b8fb27759a1
CodigoDeSenior <migueljnm840@gmail.com>
2023-10-02 05:09:38 -0300

    lc ac

---
+++ leetcode/remove_colored_pieces.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int a = 0, b = 0;
+        for (int i = 1; i < colors.size()-1; i++)
+        {
+            if (colors[i]=='A' && colors[i-1] == 'A' && colors[i+1] == 'A') a++;
+            if (colors[i]=='B' && colors[i-1] == 'B' && colors[i+1] == 'B') b++;
+        }
+
+        return a > b;
+    }
+};
