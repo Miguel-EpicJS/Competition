9b97ac1224055d3c9fe983d57c72e3b5dad271f5
CodigoDeSenior <migueljnm840@gmail.com>
2023-10-04 06:04:24 -0300

    many wa

--- codeforces/unordered/489C.cpp
+++ codeforces/unordered/489C.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int m, s;
-
+ 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-
+ 
+ 
     cin >> m >> s;
-
+ 
     if (m == 1 && s == 0)
     {
 	cout << "0 0\n";
@@ -25,22 +25,46 @@ int main()
     {
 	string s2 = "", s1 = "";
 	int ms2 = s;
-
+ 
 	while (ms2 > 0)
 	{
 	    if (ms2 >= 9) ms2 -= 9, s2 += '9';
 	    else s2 += (ms2 + '0'), ms2 = 0;
 	}
-
+ 
 	s1 = s2;
+	reverse(s1.begin(), s1.end());
+	deque<char> ss1(s1.begin(), s1.end());
+ 
+	while(ss1.front() == '0')
+	{
+	    ss1.push_back('0');
+	    ss1.pop_front();
+	}
+ 
+	string anss1(ss1.begin(), ss1.end());
+ 
+	while(anss1.size() < m) anss1 += '0';
+	while(s2.size() < m) s2 += '0';
 
-    	sort(s1.begin(), s1.end());
+	sort(anss1.begin()+1, anss1.end());	
+ 
+	int i = 0;
+	int j = anss1.size()-1;
 
-	
+	while(i < j)
+	{
+	    if ((anss1[i] == '0' && i != 0) || (i == 0 && anss1[i] == '1')) i++;
+	    else if (anss1[j] == '9') j--;
+	    else
+	    {
+		anss1[i]--;
+		anss1[j]++;
+	    }
+	}
 
-	cout << s1 << " " << s2 << "\n";
+	cout << anss1 << " " << s2 << "\n";
     }
-
+ 
     return 0;
 }
-
---
+++ codeforces/unordered/580B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long n, d;
+
+    cin >> n >> d;
+
+    vector<pair<long long, long long>> friends;
+
+    long long maxx = 0;
+    for (int i = 0; i < n; i++)
+    {
+	long long x, y;
+	cin >> x >> y;
+
+	friends.push_back({x, y});
+	maxx = max(maxx, y);
+    }
+
+    sort(friends.begin(), friends.end());
+
+
+    int p1 = 0;
+
+    long long curr = 0;
+
+
+    for (int i = 0; i < n; i++)
+    {
+
+	if (friends[i].first-friends[p1].first < d )
+	{
+	    curr+=friends[i].second;
+	    maxx = max(curr, maxx);
+	}
+	else 
+	{
+	    while(friends[i].first-friends[p1].first >= d)
+	    {
+		curr -= friends[p1].second;
+		p1++;
+	    }
+	}
+    }
+
+    cout << maxx << "\n";
+
+    return 0;
+}
+
