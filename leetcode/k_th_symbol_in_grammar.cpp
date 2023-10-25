1f8efc4c8291ef8f0f9b4f10c4e487fe1ea65b4b
CodigoDeSenior <migueljnm840@gmail.com>
2023-10-25 05:42:34 -0300

    https://leetcode.com/problems/k-th-symbol-in-grammar/submissions/\?envType\=daily-question\&envId\=2023-10-25

---
+++ leetcode/k_th_symbol_in_grammar.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int recursion(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+
+        int totalElements = pow(2, (n - 1));
+        int halfElements = totalElements / 2;
+
+        if (k > halfElements) {
+            return 1 - kthGrammar(n, k - halfElements);
+        }
+
+        return recursion(n - 1, k);
+    }
+
+    int kthGrammar(int n, int k) {
+        
+        return recursion(n, k);    
+
+    }
+};
