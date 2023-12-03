cdc9a7d76f5f04a934f861742dc2ebf267774f9a
CodigoDeSenior <migueljnm840@gmail.com>
2023-12-03 08:38:40 -0300

    https://leetcode.com/problems/minimum-time-visiting-all-points/submissions/\?envType\=daily-question\&envId\=2023-12-03

---
+++ leetcode/minimum_time_visiting_all_points.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        long long ans = 0;
+
+        for (int i = 1; i < points.size(); i++)
+        {
+            ans += max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+        }
+
+        return ans;
+    }
+};
