0fa533ed637a3ea0662bc908070109902322e0e7
CodigoDeSenior <migueljnm840@gmail.com>
2024-01-05 07:12:37 -0300

    https://leetcode.com/problems/longest-increasing-subsequence/submissions/\?envType\=daily-question\&envId\=2024-01-05

---
+++ leetcode/lis.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int lengthOfLIS(vector<int>& nums)
+    {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
+    }
+
+};
